ba685c32d56f7c6b98b51ec4c83f048299349766
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2021-05-31 00:38:13 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,55 +13,55 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        List<String> nameThatWebsites = new ArrayList<>();
-//        System.out.println("                           (o)(o)\n" +
-//                "                          /     \\\n" +
-//                "                         /       |\n" +
-//                "                        /   \\  * |\n" +
-//                "          ________     /    /\\__/\n" +
-//                "  _      /        \\   /    /\n" +
-//                " / \\    /  ____    \\_/    /\n" +
-//                "//\\ \\  /  /    \\         /\n" +
-//                "V  \\ \\/  /      \\       /\n" +
-//                "    \\___/        \\_____/");
-//        System.out.println("***ULAKE PROJECT COOPERATION***");
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println("------------------------");
-//        System.out.println("Welcome to Worm Crawler");
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        List<String> nameThatWebsites = new ArrayList<>();
+        System.out.println("                           (o)(o)\n" +
+                "                          /     \\\n" +
+                "                         /       |\n" +
+                "                        /   \\  * |\n" +
+                "          ________     /    /\\__/\n" +
+                "  _      /        \\   /    /\n" +
+                " / \\    /  ____    \\_/    /\n" +
+                "//\\ \\  /  /    \\         /\n" +
+                "V  \\ \\/  /      \\       /\n" +
+                "    \\___/        \\_____/");
+        System.out.println("***ULAKE PROJECT COOPERATION***");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("------------------------");
+        System.out.println("Welcome to Worm Crawler");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         ConfigurationManager configurationManager = ConfigurationManager.getInstance();
         configurationManager.authenticate();
-//        System.out.println("Authenticated!");
-//        System.out.println("*********************************************************");
-//        System.out.println("What do you wanna search ?");
-//        String keyword = new Scanner(System.in).nextLine();
-//        System.out.println("How many website you wanna scrap ? ");
-//        int n = new Scanner(System.in).nextInt();
-//        System.out.println("Name of those..");
-//        for (int i = 0; i < n; i++) {
-//            String target = new Scanner(System.in).nextLine();
-//            nameThatWebsites.add(target);
-//        }
-//        System.out.println("\n-----------------------------------------");
-//        configurationManager.setTopic(keyword);
-//        configurationManager.addWebsitesTarget(nameThatWebsites);
-//        System.out.println("Begin scrapped");
-//        configurationManager.scrapped();
-//        System.out.println("End scrapped");
-//        System.out.println("Begin download");
-//        configurationManager.downloadToWorkPlace();
-//        System.out.println("********************************");
-//        System.out.println("End download");
-//        System.out.println("Begin unpack");
-//        configurationManager.openSources();
-//        System.out.println("End unpack");
+        System.out.println("Authenticated!");
+        System.out.println("*********************************************************");
+        System.out.println("What do you wanna search ?");
+        String keyword = new Scanner(System.in).nextLine();
+        System.out.println("How many website you wanna scrap ? ");
+        int n = new Scanner(System.in).nextInt();
+        System.out.println("Name of those..");
+        for (int i = 0; i < n; i++) {
+            String target = new Scanner(System.in).nextLine();
+            nameThatWebsites.add(target);
+        }
+        System.out.println("\n-----------------------------------------");
+        configurationManager.setTopic(keyword);
+        configurationManager.addWebsitesTarget(nameThatWebsites);
+        System.out.println("Begin scrapped");
+        configurationManager.scrapped();
+        System.out.println("End scrapped");
+        System.out.println("Begin download");
+        configurationManager.downloadToWorkPlace();
+        System.out.println("********************************");
+        System.out.println("End download");
+        System.out.println("Begin unpack");
+        configurationManager.openSources();
+        System.out.println("End unpack");
     }
 }
